6bc9e299705913c35a2dff552d655d3849e60e3f
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-05-24 04:16:43 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -46,17 +46,18 @@ class WZMLStyle:
 
 ┌<b><i>BOT STATUS</i><b>
 └ <b><i>RAM ( MEMORY ) :</i></b>
-  ├[{ram_bar} {ram}%]
-  └<b>U :</b> {ram_u} | <b>F :</b> {ram_f} | <b>T :</b> {ram_t}
-  ┌<b><i>SWAP MEMORY :</i></b>
-  ├ [{swap_bar} {swap}%]
-  └<b>U :</b> {swap_u} | <b>F :</b> {swap_f} | <b>T :</b> {swap_t}
+├[{ram_bar} {ram}%]
+└<b>U :</b> {ram_u} | <b>F :</b> {ram_f} | <b>T :</b> {ram_t}
 
-  ┌<b><i>DISK :</i></b>
-  ├ {disk_bar} {disk}%
-  ├ <b>Total Disk Read :</b> {disk_read}
-  ├ <b>Total Disk Write :</b> {disk_write}
-  └<b>U :</b> {disk_u} | <b>F :</b> {disk_f} | <b>T :</b> {disk_t}
+┌<b><i>SWAP MEMORY :</i></b>
+├ [{swap_bar} {swap}%]
+└<b>U :</b> {swap_u} | <b>F :</b> {swap_f} | <b>T :</b> {swap_t}
+
+┌<b><i>DISK :</i></b>
+├ {disk_bar} {disk}%
+├ <b>Total Disk Read :</b> {disk_read}
+├ <b>Total Disk Write :</b> {disk_write}
+└<b>U :</b> {disk_u} | <b>F :</b> {disk_f} | <b>T :</b> {disk_t}
     
     '''
     SYS_STATS = '''⌬ <b><i>OS SYSTEM :</i></b>
@@ -137,7 +138,7 @@ class WZMLStyle:
     L_LOG_START =           "<b><u>Leech Started :</u></b>\n┃\n├<b>User :</b> {mention} ( #ID{uid} )\n└<b>Source :</b> <a href='{msg_link}'>Click Here</a>"
 
     # async def onUploadComplete(): ---> tasks_listener.py
-    NAME =                  '<code>{Name}</code>\n┃\n'
+    NAME =                  '<code><i>{Name}</i></code>\n┃\n'
     SIZE =                  '├<b>Size: </b>{Size}\n'
     ELAPSE =                '├<b>Elapsed: </b>{Time}\n'
     MODE =                  '├<b>Mode: </b>{Mode}\n'
@@ -201,7 +202,7 @@ class WZMLStyle:
     NON_ENGINE =     '\n├<b>Engine:</b> {Engine}'
 
     ####--------OVERALL MSG FOOTER----------
-    USER =              '\n├<b>User:</b> {NAME} ( <code>{ID}</code> ) | '
+    USER =              '\n├<b>User:</b> {NAME} | '
     ID =                                                        '<b>ID:</b> <code>{Id}</code>'
     BTSEL =          '\n├<b>Select:</b> {Btsel}'
     CANCEL =         '\n└<code>{Cancel}</code>\n\n'
