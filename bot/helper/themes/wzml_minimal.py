c7e3ede722ffe320c296b2c61aa2f5b22167454d
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-05-24 04:23:53 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -138,7 +138,7 @@ class WZMLStyle:
     L_LOG_START =           "<b><u>Leech Started :</u></b>\n┃\n├<b>User :</b> {mention} ( #ID{uid} )\n└<b>Source :</b> <a href='{msg_link}'>Click Here</a>"
 
     # async def onUploadComplete(): ---> tasks_listener.py
-    NAME =                  '<code><i>{Name}</i></code>\n┃\n'
+    NAME =                  '<b><i>{Name}</i></b>\n┃\n'
     SIZE =                  '├<b>Size: </b>{Size}\n'
     ELAPSE =                '├<b>Elapsed: </b>{Time}\n'
     MODE =                  '├<b>Mode: </b>{Mode}\n'
